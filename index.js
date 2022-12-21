8272e8bb1437c15f948536031a9c42350029b16a
vsunday <vsunday@users.noreply.github.com>
2022-12-21 11:21:50 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const boxSDK = require("box-node-sdk");
+
+const app = express();
+
+app.use(express.urlencoded({ extend: false }));
+
+app.post("/lockfolder", async (req, res) => {
+  try {
+    // clientIDと、clientSecretは、構成のクライアントIDとクライアント機密コード
+    const sdk = new boxSDK({
+      clientID: process.env.clientID,
+      clientSecret: process.env.clientSecret
+    });
+
+    const authCode = req.body.authCode;
+    const folderId = req.body.folderId;
+
+    const tokenInfo = await sdk.getTokensAuthorizationCodeGrant(authCode);
+    const client = sdk.getBasicClient(tokenInfo.accessToken);
+
+    // 受け取った対象のファイルに対してロックをかける
+    await client.folders.lock(folderId);
+
+    res.status(200).json({
+      message: "folder locked",
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+const PORT = process.env.PORT || "4000";
+app.listen(PORT, (result) => {
+  console.log("Express Server started on port: " + PORT);
+});
